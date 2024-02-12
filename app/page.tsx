9337c5642d5a451b2a4af643be060e9513ed4e9d
Ojaspj <prajwaljoshi00@gmail.com>
2024-02-12 23:02:00 +0545

    build: new home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaArrowDown, FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div className="w-full h-screen text-center bg-amber-50">
-      <div className="text-gray-800 max-w-[1240px] w-full h-full mx-auto p-2 flex flex-col justify-center items-center">
+      <div className=" relative text-gray-800 max-w-[1240px] w-full h-full mx-auto p-2 flex flex-col justify-center items-center">
         <div>
           <p className="uppercase text-md">Hello World!</p>
           <h1 className=" py-4 text-[70px] font-bold ">
@@ -18,28 +18,41 @@ export default function Home() {
           </p>
         </div>
 
-        <div className="flex gap-4  justify-between items-center">
-          <div className="w-[600px] border-b border-gray-800 " />
-          {/* social links  */}
-          <div className="flex gap-4">
-            <a
-              href={"https://www.linkedin.com/in/prajwal-joshi-3b3734156/"}
-              target="_blank"
-            >
-              <FaLinkedin size={30} />
-            </a>
-            <a href={"https://github.com/Ojaspj"} target="_blank">
-              <FaGithub size={30} />
-            </a>
-            <a
-              href="https://drive.google.com/file/d/1EDs7c0bvfIltq2FFK1OC6hEbNCvjLHP-/view?usp=sharing"
-              target="_blank"
-              className="rounded-full p-1 border border-gray-800"
-            >
-              <p className="font-bold text-sm">CV</p>
-            </a>
+        <div className=" absolute bottom-8">
+          {/* arrow down */}
+          <div className="mb-6 flex flex-col items-center">
+            <div className=" animate-bounce flex flex-col items-center">
+              <FaArrowDown size={40} />
+            </div>
+
+            <div className=" font-normal p-2 border border-gray-800 animate-pulse  rounded-xl shadow-sm">
+              <h1>Scroll Down</h1>
+            </div>
           </div>
-          <div className="w-[600px] border-b border-gray-800" />
+
+          <div className="flex gap-4  justify-between items-center">
+            <div className="w-[600px] border-b border-gray-800 " />
+            {/* social links  */}
+            <div className="flex gap-4">
+              <a
+                href={"https://www.linkedin.com/in/prajwal-joshi-3b3734156/"}
+                target="_blank"
+              >
+                <FaLinkedin size={30} />
+              </a>
+              <a href={"https://github.com/Ojaspj"} target="_blank">
+                <FaGithub size={30} />
+              </a>
+              <a
+                href="https://drive.google.com/file/d/1EDs7c0bvfIltq2FFK1OC6hEbNCvjLHP-/view?usp=sharing"
+                target="_blank"
+                className="rounded-full p-1 border border-gray-800"
+              >
+                <p className="font-bold text-sm">CV</p>
+              </a>
+            </div>
+            <div className="w-[600px] border-b border-gray-800" />
+          </div>
         </div>
       </div>
     </div>
