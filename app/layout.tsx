ab312593dd1c45692195f99e7ccdc84e8decf920
Ojaspj <prajwaljoshi00@gmail.com>
2024-02-12 23:45:31 +0545

    feat : under dev messsage to content two

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-amber-50 text-gray-800`}>
+        {children}
+      </body>
     </html>
   );
 }
