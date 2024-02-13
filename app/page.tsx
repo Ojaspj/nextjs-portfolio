7c74b6c803ac3be7efd0ef1f47190a51007c35b2
Ojaspj <prajwaljoshi00@gmail.com>
2024-02-13 23:06:54 +0545

    feat: final new tabs components

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ import {
   BackgroundBeamsDemo,
 } from "@/src/components/ui/bg-beams";
 import { LampDemo } from "@/src/components/ui/lamp";
-import { Tabs } from "@/src/components/ui/tabs";
-import { FaArrowDown, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaReact, FaPython, FaLaptopCode } from "react-icons/fa";
+import { SiFlutter } from "react-icons/si";
 
 export default function Home() {
   return (
@@ -15,7 +15,19 @@ export default function Home() {
       {/* <Content /> */}
       <LampDemo />
       {/* <BackgroundBeamsDemo /> */}
-      <div className="h-auto w-full bg-slate-950">
+      <div className="h-auto w-full bg-slate-950 relative">
+        <div className=" absolute top-60 left-32">
+          <FaReact className="text-slate-700" size={100} />
+        </div>
+
+        <div className=" absolute top-96 left-32">
+          <FaLaptopCode className="text-slate-700 " size={100} />
+        </div>
+
+        <div className=" absolute left-32 bottom-40 ">
+          <SiFlutter className="text-slate-700 " size={100} />
+        </div>
+
         <TabsDemo />
       </div>
 
